37cf204ab193ead03e0fd572b4f1204cc1156dc1
PetriJF <jamespetri28@gmail.com>
2023-01-31 01:33:02 +0200

    Cleaned up the code a bit

--- src/leg_controller/leg_controller/servo_move.py
+++ src/leg_controller/leg_controller/servo_move.py
@@ -15,13 +15,6 @@ class hexMotorControl(Node):
     leftLegsAddr: float
     rightLegsAddr: float
 
-    #LB = legReferencing(False, 0, 1, 2, "Leg: <Left Back>")
-    #LM = legReferencing(False, 3, 4, 5, "Leg: <Left Middle>")
-    #LF = legReferencing(False, 6, 7, 8, "Leg: <Left Front>")
-    #RB = legReferencing(True, 0, 1, 2, "Leg: <Right Back>")
-    #RM = legReferencing(True, 3, 4, 5, "Leg: <Right Middle>")
-    #RF = legReferencing(True, 6, 7, 8, "Leg: <Right Front>")
-
     def __init__(self):
         super().__init__("hex_legs_controller")
         # TODO: Add the addresses of the 2 i2c controllers
