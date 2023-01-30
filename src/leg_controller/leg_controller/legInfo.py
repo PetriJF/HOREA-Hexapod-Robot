ff2de929f6d21f7e720438e01134b8bff77990c7
PetriJF <jamespetri28@gmail.com>
2023-01-31 01:54:41 +0200

    Added origin referencing for each leg

--- src/leg_controller/leg_controller/legInfo.py
+++ src/leg_controller/leg_controller/legInfo.py
@@ -1,21 +1,44 @@
 #!/usr/bin/env python3
 from dataclasses import dataclass
+import numpy as np
+
+BASE_WIDTH = 65.0 # mm
+
+@dataclass
+class Point:
+    x: float
+    y: float    
+    z: float
+
+@dataclass
+class Angles:
+    hip: float
+    shoulder: float    # height
+    knee: float
 
 @dataclass
 class legReferencing:
     """A class used to organize the 6 legs and their respective 18 servo motors in terms of side and motor connection on the I2C board. 
        NOTE: side = false represents left, side = true represents right"""
+    # Represents the side of the legs
     side: bool
     
+    # Origin representation
+    originX: float
+    originY: float
+
+    # Represents the port on the board for each motor of a leg
     hip: int
     shoulder: int
     knee: int
 
+    # Simple description of the leg
     description: str
 
-LB = legReferencing(False, 0, 1, 2, "Leg: <Left Back>")
-LM = legReferencing(False, 3, 4, 5, "Leg: <Left Middle>")
-LF = legReferencing(False, 6, 7, 8, "Leg: <Left Front>")
-RB = legReferencing(True, 0, 1, 2, "Leg: <Right Back>")
-RM = legReferencing(True, 3, 4, 5, "Leg: <Right Middle>")
-RF = legReferencing(True, 6, 7, 8, "Leg: <Right Front>")
+# Leg initializations
+LB = legReferencing(False, BASE_WIDTH * round(np.cosd(7.0*np.pi/6.0),2), BASE_WIDTH * round(np.sind(7*np.pi/6.0),2),0, 1, 2, "Leg: <Left Back>")
+LM = legReferencing(False, BASE_WIDTH * round(np.cosd(3.0*np.pi/2.0),2), BASE_WIDTH * round(np.sind(3.0*np.pi/2.0),2), 3, 4, 5, "Leg: <Left Middle>")
+LF = legReferencing(False, BASE_WIDTH * round(np.cosd(11.0*np.pi/6.0),2), BASE_WIDTH * round(np.sind(11.0*np.pi/6.0),2), 6, 7, 8, "Leg: <Left Front>")
+RB = legReferencing(True, BASE_WIDTH * round(np.cosd(5.0*np.pi/6.0),2), BASE_WIDTH * round(np.sind(5.0*np.pi/6.0),2), 0, 1, 2, "Leg: <Right Back>")
+RM = legReferencing(True, BASE_WIDTH * round(np.cosd(np.pi/2.0),2), BASE_WIDTH * round(np.sind(np.pi/2.0),2), 3, 4, 5, "Leg: <Right Middle>")
+RF = legReferencing(True, BASE_WIDTH * round(np.cosd(np.pi/6.0),2), BASE_WIDTH * round(np.sind(np.pi/6.0),2), 6, 7, 8, "Leg: <Right Front>")
