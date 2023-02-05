1099209ad3af07277b41e7044555380373ec8527
PetriJF <jamespetri28@gmail.com>
2023-02-06 00:51:36 +0200

    Fixed the bug. Wrong centering on servos

--- src/build/hexapod_interfaces/ament_cmake_python/hexapod_interfaces/hexapod_interfaces/msg/__init__.py
+++
@@ -1,1 +0,0 @@
-from hexapod_interfaces.msg._target_angles import TargetAngles  # noqa: F401
--- src/build/leg_controller/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/pi/hexapod/src/install/hexapod_interfaces/share/hexapod_interfaces/package.sh"
