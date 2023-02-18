ebd07a2e29253a8106da37d73811eb935eb17b49
PetriJF <jamespetri28@gmail.com>
2023-02-19 00:50:15 +0200

    Parameterized all the nodes where relevant

--- src/hexapod_bringup/launch/fullRun.launch.py
+++ src/hexapod_bringup/launch/fullRun.launch.py
@@ -3,19 +3,43 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    # Robot constants
+    COXA_LEN = 71.5
+    FEMUR_LEN = 100.02
+    FOOT_HEIGHT = 0.0
+    TIBIA_LEN = 150 + FOOT_HEIGHT
+    GAIT_ALTITUDE = 90.0
+    BASE_WIDTH = 65.0
+    GAIT_WIDTH = 300.0
+    
     ld = LaunchDescription()
 
     servo_node = Node(
         package = "leg_controller",
-        executable = "servo_node"
+        executable = "servo_node",
     )
     kin_node = Node(
         package = "leg_controller",
-        executable = "kin_node"
+        executable = "kin_node",
+        parameters = [
+            {"coxa_len": COXA_LEN},
+            {"femur_len": FEMUR_LEN},
+            {"tibia_len": TIBIA_LEN},
+            {"gait_altitude": GAIT_ALTITUDE},
+            {"base_width": BASE_WIDTH}
+        ]
     )
     ctrl_node = Node(
         package = "leg_controller",
-        executable = "ctrl_node"
+        executable = "ctrl_node",
+        parameters = [
+            {"coxa_len": COXA_LEN},
+            {"femur_len": FEMUR_LEN},
+            {"tibia_len": TIBIA_LEN},
+            {"gait_altitude": GAIT_ALTITUDE},
+            {"base_width": BASE_WIDTH},
+            {"gait_width": GAIT_WIDTH}
+        ]
     )
 
     ld.add_action(servo_node)
