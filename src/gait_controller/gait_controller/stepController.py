01492a2510cef3caaa90653bcbfface764cbf9b3
PetriJF <jamespetri28@gmail.com>
2023-02-27 19:17:44 +0200

    Added the subscription. Need to do the control for each command

--- src/gait_controller/gait_controller/stepController.py
+++ src/gait_controller/gait_controller/stepController.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int64
+from std_msgs.msg import Int64, String
 
 ## Bézier <3
 class Stepper(Node):
     def __init__(self):
         super().__init__("step_ctrl_node")
         self.get_logger().info("Init")
+        self.posSub = self.create_subscription(String, 'stepCommand', self.stepCommand, 10)
+        # Change to a float array
         self.command_ = self.create_publisher(Int64, 'StepCommands', 10)
 
-    def stepCommand(self, cmd = Int64):
+    def stepCommand(self, cmd = String):
+        # We need: step angle, step length, gait_altitude, gait_width!
+        if cmd == "w":
+            pass
+        elif cmd == "a":
+            pass
+        elif cmd == "s":
+            pass
+        elif cmd == "d":
+            pass
+        elif cmd == "q":
+            pass
+        elif cmd == "e":
+            pass
+        else:
+            self.get_logger().warning("Incorrect command sent to the step controller!!")
+        
         self.command_.publish(cmd)
 
         
