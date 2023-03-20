fcf61bab9e42ae92a420f69a1140d7236d664317
PetriJF <jamespetri28@gmail.com>
2023-03-20 16:40:58 +0200

    Added basic code to read from the IMU

--- src/sensory_controller/sensory_controller/imu.py
+++ src/sensory_controller/sensory_controller/imu.py
@@ -1,21 +1,41 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+import qwiic_icm20948
+from time import *
 
 class IMUNode(Node):
     def __init__(self):
         super().__init__("imu_node")
 
+        self.IMU = qwiic_icm20948.QwiicIcm20948()
+        if not self.IMU.connected:
+            self.get_logger().error("The Qwiic ICM20948 device isn't connected to the system. Please check your connection")
+            return
 
-        self.get_logger().info("Init")
+        self.IMU.begin()
+
+        self.get_logger().info("IMU node initialized successfully")
     
     def gyroscopeRead(self):
-        gyroRead = self.sense.get_orientation()
-        pitch = round(gyroRead["pitch"], 2) # front to back
-        roll = round(gyroRead["roll"], 2)   # side to side
-        yaw = round(gyroRead["yaw"], 2)     # around center axis
-    
-        self.get_logger().info("Orientation: " + pitch + " " + roll + " " + yaw + "\n")
+        if self.IMU.dataReady():
+            self.IMU.getAgmt() # read all axis and temp from sensor, note this also updates all instance variables
+            print(\
+                #'{: 06d}'.format(self.IMU.axRaw)\
+                #, '\t', '{: 06d}'.format(self.IMU.ayRaw)\
+                #, '\t', '{: 06d}'.format(self.IMU.azRaw)\
+                 '\n gX: {: 4.1f}'.format(self.IMU.gxRaw)\
+                , '\n gY: {: 4.1f}'.format(self.IMU.gyRaw)\
+                , '\n gX: {: 4.1f}'.format(self.IMU.gzRaw)\
+                #, '\t', '{: 06d}'.format(self.IMU.mxRaw)\
+                #, '\t', '{: 06d}'.format(self.IMU.myRaw)\
+                #, '\t', '{: 06d}'.format(self.IMU.mzRaw)\
+            )
+            sleep(0.5)
+        else:
+            print("Waiting for data")
+            sleep(0.5)
+
 
 def main(args = None):
     rclpy.init(args = args)
