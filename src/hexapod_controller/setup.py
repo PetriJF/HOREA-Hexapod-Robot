df3f277fbe9a6c0192db0ceaf7b95fe8ff0e844a
PetriJF <jamespetri28@gmail.com>
2023-03-25 02:49:51 +0200

    Renamed keyboard teleop

--- src/hexapod_controller/setup.py
+++ src/hexapod_controller/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "teleop_node = hexapod_controller.hexapod_teleop_controller:main"
+            "teleop_keyboard_node = hexapod_controller.keyboard_teleop_controller:main"
         ],
     },
 )
